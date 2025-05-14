f66c8f36a8f9f91767eae3a45a9169b6abbd23ad
Iman <ST10464928@rcconnect.edu.za>
2025-05-14 13:50:01 +0200

    nonsense

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,6 +12,41 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+        }import java.util.Scanner;
+
+        public class Main {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                // == Register ==
+                System.out.println("== Register a New User ==");
+
+                System.out.println("Enter first name: ");
+                String firstName = scanner.nextLine();
+
+                System.out.println("Enter last name: ");
+                String lastName = scanner.nextLine();
+
+                System.out.println("Enter username (must include '_' and be no more than 5 characters long): ");
+                String username = scanner.nextLine();
+
+                System.out.println("Enter password (must have 1 uppercase, 1 digit, 1 special char, and be >= 8 char): ");
+                String password = scanner.nextLine();
+
+                System.out.println("Enter cell number (+27XXXXXXXXX): ");
+                String cellNumber = scanner.nextLine();
+
+                Login login = new Login(firstName, lastName, username, password,cellNumber);
+                String registrationResult = login.registerUser();
+                System.out.println(registrationResult);
+
+                if (!registrationResult.equals("User has been registered successfully.")) {
+                    System.out.println("Username or password-incorrect. Exiting...");
+                    return;
+                }
+
+                System.out.println("Welcome to Quickchat!");
+            }
         }
     }
 }
